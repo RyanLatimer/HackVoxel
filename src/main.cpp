a13bf5a2e84ec70838a7fc8a532041b53cdc05b4
Ryan Latimer <rlatimer96@harborps.org>
2025-06-19 14:34:55 -0400

    Add glm library

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
